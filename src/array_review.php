a1333bd846ea8c8f53b5635f9faa8e6f8486a048
JakeLandowski <jakelandowski@gmail.com>
2018-01-16 09:27:32 -0800

    Part 2 done : function that sorts and prints animals

--- src/array_review.php
+++ src/array_review.php
@@ -4,3 +4,15 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $animals = ['panda', 'alpaca', 'boa'];
+
+function sortAndPrintAnimals($animals)
+{
+    sort($animals);
+
+    foreach($animals as $animal)
+    {
+        echo "$animal&nbsp;";  
+    }
+
+    echo "<br />";
+}
